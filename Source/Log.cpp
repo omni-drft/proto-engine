2358ec44ad078991a1d4e04807aa61ac6d374d83
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-03-04 22:31:34 +0100

    Adding XML documentation to Log class

--- Source/Log.cpp
+++ Source/Log.cpp
@@ -1,13 +1,24 @@
+/* ============================ FILE PURPOSE ==============================
+* 
+*  This file contains the implementation of the Log class. 
+* 
+========================================================================= */
+
 #include "Log.hpp"
 
 namespace ProtoEngine
 {
+	/// <summary>
+	/// Global logger instance
+	/// </summary>
 	std::shared_ptr<spdlog::logger> Log::logger;
 
 	void Log::Init()
 	{
+		// Set the log pattern (time, log level, message)
 		spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
 		logger = spdlog::stdout_color_mt("ProtoEngine");
+		// Set the log level depending on the build type
 		#ifdef DEBUG
 		logger->set_level(spdlog::level::trace);
 		#else
