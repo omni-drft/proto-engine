4145ff384b853f635991c518c0dda0c8d1dc4a88
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-03-10 18:37:28 +0100

    Improving class architecture and moving on with vulkan setup

--- include/window.h
+++ include/window.h
@@ -2,32 +2,54 @@
 #define PROTOENGINE_INCLUDE_WINDOW_H_
 
 #include <cstdint>
+#include <vector>
 
 #include "engine_exports.h"
 
 namespace ProtoEngine {
-class ENGINE_API Window {
+class Window {
  public:
-  Window(uint16_t width, uint16_t height);
-  void Init();
-  void PollEvents();
-  bool ShouldClose();
-  void Close();
+  ENGINE_API Window(uint16_t width, uint16_t height);
+  ENGINE_API void Init();
+  ENGINE_API void PollEvents();
+  ENGINE_API bool ShouldClose();
+  ENGINE_API void Close();
 
-  void DrawTriangle();
+  ENGINE_API void DrawTriangle();
 
-  ~Window();
+  ENGINE_API ~Window();
 
  private:
+
+  bool CheckValidationLayerSupport();
+
+  void InitGLFW();
+  void InitVulkan();
+  void PickPhysicalDevice();
+
   uint16_t width_, height_;
   bool resizable_;
-  
+
   struct GLFWWindowHandle;
   GLFWWindowHandle* window_;
-  
+
   struct VkInstanceHandle;
   VkInstanceHandle* vulkan_instance_;
+
+  struct VkDeviceHandle;
+  VkDeviceHandle* display_device_;
+
+  const std::vector<const char*> validation_layers_{
+      "VK_LAYER_KHRONOS_validation"
+  };
+
+#ifdef _DEBUG
+  const bool enable_validation_layers_{true};
+#else
+  const bool enable_validation_layers_{false};
+#endif
 };
 
 }  // namespace ProtoEngine
+
 #endif  // PROTOENGINE_INCLUDE_WINDOW_H_
--- source/window.cc
+++ source/window.cc
@@ -21,6 +21,10 @@ struct ProtoEngine::Window::VkInstanceHandle {
   VkInstance instance = nullptr;
 };
 
+struct ProtoEngine::Window::VkDeviceHandle {
+  VkPhysicalDevice physical_device = VK_NULL_HANDLE;
+};
+
 void error_callback(int error, const char* description) {
   std::cerr << "GLFW Error: " << description << std::endl;
 }
@@ -30,54 +34,12 @@ ProtoEngine::Window::Window(uint16_t width, uint16_t height)
       height_(height),
       resizable_(false),
       window_(new GLFWWindowHandle()),
-      vulkan_instance_(new VkInstanceHandle()) {}
+      vulkan_instance_(new VkInstanceHandle()),
+      display_device_(new VkDeviceHandle()) {}
 
 void ProtoEngine::Window::Init() {
-  if (!glfwInit()) {
-    throw std::runtime_error("Failed to initialise GLFW!");
-  }
-
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-  glfwSetErrorCallback(error_callback);
-
-  window_->window =
-      glfwCreateWindow(width_, height_, "ProtoEngine Window", nullptr, nullptr);
-
-  if (!window_) {
-    throw std::runtime_error("Failed to create window!");
-  }
-
-  VkApplicationInfo app_info;
-  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  app_info.pApplicationName = "Hello Triangle";
-  app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-  app_info.pEngineName = "No Engine";
-  app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-  app_info.apiVersion = VK_API_VERSION_1_0;
-
-  VkInstanceCreateInfo create_info{};
-  create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  create_info.pApplicationInfo = &app_info;
-
-  uint32_t glfw_extension_count{};
-  const char** glfw_extensions;
-
-  glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
-
-  create_info.enabledExtensionCount = glfw_extension_count;
-  create_info.ppEnabledExtensionNames = glfw_extensions;
-  create_info.enabledLayerCount = 0;
-
-  VkResult result{
-      vkCreateInstance(&create_info, nullptr, &vulkan_instance_->instance)};
-
-
-
-  glm::vec4 vec;
-  glm::mat4 matrix;
-  auto test{matrix * vec};
+  InitGLFW();
+  InitVulkan();
 }
 
 void ProtoEngine::Window::PollEvents() { glfwPollEvents(); }
@@ -97,4 +59,88 @@ void ProtoEngine::Window::DrawTriangle() {}
 ProtoEngine::Window::~Window() {
   delete window_;
   delete vulkan_instance_;
+  delete display_device_;
+}
+
+bool ProtoEngine::Window::CheckValidationLayerSupport() {
+  uint32_t layer_count{};
+  vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+  std::vector<VkLayerProperties> available_layers(layer_count);
+  vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+
+  for (const char* layer_name : validation_layers_) {
+    bool layer_found{false};
+    for (const auto& layer_properties : available_layers) {
+      if (strcmp(layer_name, layer_properties.layerName) == 0) {
+        layer_found = true;
+        break;
+      }
+    }
+    if (!layer_found) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void ProtoEngine::Window::InitGLFW() {
+  if (!glfwInit()) {
+    throw std::runtime_error("Failed to initialise GLFW!");
+  }
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  glfwSetErrorCallback(error_callback);
+
+  window_->window =
+      glfwCreateWindow(width_, height_, "ProtoEngine Window", nullptr, nullptr);
+
+  if (!window_) {
+    throw std::runtime_error("Failed to create window!");
+  }
+}
+
+void ProtoEngine::Window::InitVulkan() {
+  if (enable_validation_layers_ && !CheckValidationLayerSupport()) {
+    throw std::runtime_error("Validation layers requested, but not available.");
+  }
+
+  VkApplicationInfo app_info;
+  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  app_info.pApplicationName = "ProtoEngine Game";
+  app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  app_info.pEngineName = "ProtoEngine";
+  app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  app_info.apiVersion = VK_API_VERSION_1_0;
+  app_info.pNext = NULL;
+
+  VkInstanceCreateInfo create_info{};
+  create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  create_info.pApplicationInfo = &app_info;
+  create_info.pNext = NULL;
+
+  if (enable_validation_layers_) {
+    create_info.enabledLayerCount =
+        static_cast<uint32_t>(validation_layers_.size());
+    create_info.ppEnabledLayerNames = validation_layers_.data();
+  } else {
+    create_info.enabledExtensionCount = 0;
+  }
+
+  vkCreateInstance(&create_info, nullptr, &vulkan_instance_->instance);
+}
+
+void ProtoEngine::Window::PickPhysicalDevice() {
+  uint32_t device_count{};
+  vkEnumeratePhysicalDevices(vulkan_instance_->instance, &device_count,
+                             nullptr);
+  if (device_count == 0) {
+    throw std::runtime_error("Failed to find GPUs with Vulkan support");
+  }
+
+  std::vector<VkPhysicalDevice> devices(device_count);
+  vkEnumeratePhysicalDevices(vulkan_instance_->instance, &device_count,
+                             devices.data());
 }
